27000849e66a5275adb820fe2933fa73add86447
Edgar <elindo586@gmail.com>
2022-05-11 12:04:19 -0400

    updated pages- formulas not working

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+import "../styles/general.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>2022 Edgar </p>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
