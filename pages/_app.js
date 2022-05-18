a1686af173b405381c81c7abc07aa61eb4ea32eb
Edgar <elindo586@gmail.com>
2022-05-18 14:43:36 -0400

    still not working

--- pages/_app.js
+++ pages/_app.js
@@ -7,28 +7,6 @@ import Footer from "./components/footer";
 import MainNav from "./components/nav-main";
 import Script from "next/script";
 
-// Pages
-import Interactive from "./interactive";
-import Formulas from "./fluid-power-formulas";
-import Inventory from "./inventory";
-import Thanks from "./thankyou";
-import Contact from "./contact-us";
-
-// Products
-import DirectionalControlValves from "./products/directional-control-valves";
-import FlowControlValves from "./products/flow-control-valves";
-import Flowmeters from "./products/flowmeters";
-import GearPumps from "./products/gear-pumps";
-import GerolerMotor from "./products/geroler-motors";
-import HydraulicManifolds from "./products/hydraulic-manifolds";
-import PistonPumps from "./products/piston-pumps";
-import VaneMotors from "./products/vane-motors";
-import VanePumps from "./products/vane-pumps";
-
-// Services
-import Laboratory from "./services/fluid-analysis";
-import Preventive from "./services/preventive-maintenance-programs";
-
 function MyApp({ Component, pageProps }) {
   return (
     <div>
---
+++ pages/models/index.js
@@ -0,0 +1,4 @@
+const Parts = require("./Parts");
+const Cylinders = require("./Cylinders");
+
+module.exports = { Parts, Cylinders };
